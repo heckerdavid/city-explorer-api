2838990673915f2ede97222bbc72029ce52612db
David Hecker <HECKERDW@GMAIL.COM>
2021-09-22 20:14:04 -0700

    Added live weather API.

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const app = express();
 const PORT = process.env.PORT;
 const weather = require("./data/weather.json");
 const cors = require("cors");
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const axios = require('axios')
 
 app.use(cors());
 
-class Forcast {
+class Forecast {
   constructor(date, description) {
     this.date = date,
     this.description = description
@@ -35,25 +37,31 @@ let haveWeatherData = (city) => {
   }
 }
 
-app.get("/weather", (req, res) => {
+app.get("/weather", async (req, res) => {
   const q = req.query
   const lat = q.lat;
   const lon = q.lon;
   const searchQuery = q.searchquery;
   const dayData = [];
 
-  let current = haveWeatherData(searchQuery);
+  let weatherApiUrl = `https://api.weatherbit.io/v2.0/current?key=${WEATHER_API_KEY}&lat=${lat}&lon=${lon}&units=I`;
+
+  const liveWeatherData = await axios.get(weatherApiUrl);
+
+  let current = liveWeatherData.data
+
+  // let current = haveWeatherData(searchQuery);
 
   if (current) {
 
     for (let day of current.data){
-      dayData.push(new Forcast(day.datetime, day.weather.description));
+      dayData.push(new Forecast(day.datetime, day.weather.description));
     }
 
     res.send(dayData)
 
   } else {
-    res.send('Sorry, no data')
+    res.send('Sorry, no weather data from ' + searchQuery)
   }
 
 });
